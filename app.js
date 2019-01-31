a7bf232c2cbcdf5a8c020d1b565cc69b4a7e301d
Andressa Vieira <andressabsb01@gmail.com>
2019-01-30 20:13:01 -0500

    basic calculator

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Becoming a Vue Andressa!'
-    }
+        title: 'Calculator',
+        display: ''
+    },
+    methods: {
+        adicionar(v){
+            if(v == '='){
+                this.display = eval(this.display)
+            }else{
+                this.display += v
+            }
+        },
+        clean(){
+            this.display = ''
+        }
+    }  
 })
