1c7a77a1d12edfd0b2bce97bc352a90b8fa14e8e
Andressa Vieira <andressabsb01@gmail.com>
2019-02-21 17:53:01 -0500

    examples using props, events, requests with  axios

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,20 +2,43 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
+    <AllFriends :friends="friends" @delete="deleteFriend"/>
+    <OnlineFriends :friends="friends"/>
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+import Blogs from './Blogs'
 
 export default {
   name: 'app',
   components: {
-    Navbar
+    Navbar,
+    OnlineFriends,
+    AllFriends,
+    Blogs
   },
   data () {
     return {
-      title: 'My first vue App! :D ' 
+      title: 'My first vue App! :D ',
+      friends: [
+        {name: 'Mario', online: true},
+        {name: 'Luigi', online: false},
+        {name: 'Toad', online: true},
+        {name: 'Bowser', online: false}
+      ]
+    }
+  },
+  methods:{
+    deleteFriend(payload){
+      console.log(payload)
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name
+       })
     }
   }
 }
