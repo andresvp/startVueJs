886e4ce1934d7a8bd5c8a0187139763bbc5a4483
Andressa Vieira <andressabsb01@gmail.com>
2019-02-22 17:41:17 -0500

    add new project routing using vue router

--- myproject/src/Blogs.vue
+++ myproject/src/Blogs.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="blogs">
         <h2>Blogs</h2>
-    <div v-for="post in posts" :key="post.id">
+    <input type="text" v-model="searchTerm">
+    <div v-for="post in filteredPosts" :key="post.id">
         <h3>{{post.title}}</h3>
-        <p>{{post.body}}</p>
+        <p>{{post.body | snippet }}</p>
     </div>
     </div>
 </template>
@@ -15,11 +16,19 @@ export default {
     name: 'Blogs',
     data() {
         return {
-            posts: []
+            posts: [],
+            searchTerm: ''
         }
     },
     methods: {
 
+    },
+    computed: {
+        filteredPosts(){
+            return this.posts.filter(post =>{
+                return post.title.match(this.searchTerm)
+            })
+        }
     },
     created(){
         axios.get('https://jsonplaceholder.typicode.com/posts/')
