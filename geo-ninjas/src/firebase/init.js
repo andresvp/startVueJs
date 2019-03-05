b581b9b0cce47441356027af3f2dd300e479f698
Andressa Vieira <andressabsb01@gmail.com>
2019-03-04 20:25:26 -0500

    add firebase in project

---
+++ geo-ninjas/src/firebase/init.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDsH9VF_GhsF_xtyWen5w7-Qu2k-Q93YBY",
+    authDomain: "udemy-geo-ninjas-ee303.firebaseapp.com",
+    databaseURL: "https://udemy-geo-ninjas-ee303.firebaseio.com",
+    projectId: "udemy-geo-ninjas-ee303",
+    storageBucket: "udemy-geo-ninjas-ee303.appspot.com",
+    messagingSenderId: "132127676349"
+  };
+  const firebaseApp = firebase.initializeApp(config);
+
+  export default firebaseApp.firestore()
